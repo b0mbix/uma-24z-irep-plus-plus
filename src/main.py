bbeba17c7c566c230159ee2a994a28f9dd37e836
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-23 23:19:21 +0100

    removed fixed random_state

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def irepplusplus(file_path):
     data = read_csv_to_dataframe(file_path)
     X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['label']), data['label'], test_size=0.33, random_state=42)
 
-    model = IRepPlusPlus(random_state=1, verbose_level=2)
+    model = IRepPlusPlus(verbose_level=2)
     model.fit(X_train, y_train)
 
     predictions = model.predict(X_test)
