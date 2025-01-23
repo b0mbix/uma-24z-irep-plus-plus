4a67427928c857eaaf6556e2a8102793f63772df
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-23 16:39:03 +0100

    changed get_data, added example usage

--- src/get_data.py
+++ src/get_data.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
-DEFAULT_PATH = '/home/adrian/uma-24z-irep-plus-plus/data/raw/breast-cancer.csv'
 
-def read_csv_to_dataframe(file_path: str = DEFAULT_PATH) -> pd.DataFrame:
+def read_csv_to_dataframe(file_path: str) -> pd.DataFrame:
     """
     Reads a CSV file and converts it into a pandas DataFrame, processing the data to be used by IRep++.
 
@@ -21,38 +20,3 @@ def read_csv_to_dataframe(file_path: str = DEFAULT_PATH) -> pd.DataFrame:
         df['label'] = df['diagnosis'].map({'M': 1, 'B': 0})
         df = df.drop(columns=['diagnosis'])
     return df
-
-
-def convert_floats_to_classes(df: pd.DataFrame, no_classes: int) -> pd.DataFrame:
-    """
-    Converts float features in a DataFrame to integer classes based on the given number of classes.
-
-    Args:
-        df (pd.DataFrame): Input DataFrame with float features and a 'label' column.
-        no_classes (int): Number of classes to divide each feature into.
-
-    Returns:
-        pd.DataFrame: DataFrame with features converted to integer classes.
-    """
-    df_transformed = df.copy()
-
-    for column in df_transformed.columns:
-        if column != 'label':
-            col_min = df_transformed[column].min()
-            col_max = df_transformed[column].max()
-            bin_width = (col_max - col_min) / no_classes
-
-            bins = [col_min + i * bin_width for i in range(no_classes)] + [col_max + 1e-5]
-
-            # Transform the column
-            df_transformed[column] = pd.cut(
-                df_transformed[column],
-                bins=bins,
-                labels=range(no_classes),
-                include_lowest=True,
-                right=False
-            )
-
-            df_transformed[column] = df_transformed[column].cat.add_categories([-1]).fillna(-1).astype(int)
-
-    return df_transformed
--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
 from irepplusplus import IRepPlusPlus
-from get_data import read_csv_to_dataframe, convert_floats_to_classes
+from get_data import read_csv_to_dataframe
+from sklearn.model_selection import train_test_split
+import sys
 
 
-def main():
-    data = read_csv_to_dataframe()
-    # data = convert_floats_to_classes(data, 100)
+def irepplusplus(file_path):
+    data = read_csv_to_dataframe(file_path)
+    X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['label']), data['label'], test_size=0.33, random_state=42)
 
-    model = IRepPlusPlus()
-    model.fit(data.drop(columns=['label']), data['label'])
+    model = IRepPlusPlus(random_state=1, verbose_level=2)
+    model.fit(X_train, y_train)
 
-    print("Rules:", model.rules)
-    predictions = model.predict(data.drop(columns=['label']))
-    correct_predictions = sum(predictions == data['label'])
-    final_accuracy = correct_predictions / len(data)
+    predictions = model.predict(X_test)
+    correct_predictions = sum(predictions == y_test)
+    final_accuracy = correct_predictions / len(y_test)
     print(f'Final accuracy: {final_accuracy}')
 
 
 if __name__ == '__main__':
-    main()
+    irepplusplus(sys.argv[1])
