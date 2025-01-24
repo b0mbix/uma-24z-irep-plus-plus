a0b92152c98c6d786329fd195f22146e139daa1d
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-24 19:29:16 +0100

    added irep

--- src/irep.py
+++ src/irep.py
@@ -2,15 +2,16 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import logging
+import math
 
 
 class IRep:
-    def __init__(self, max_iterations: int = 10, max_conditions_in_rule: int = 5, test_percentage: float = 2/3, random_state: int = None, verbose_level: int = 0):
+    def __init__(self, max_iterations: int = 20, max_conditions_in_rule: int = 5, prune_percentage: float = 1/3, random_state: int = None, verbose_level: int = 0):
         self.max_iterations = max_iterations
-        self.test_percentage = test_percentage
+        self.test_percentage = prune_percentage
         self.max_conditions_in_rule = max_conditions_in_rule
         self.random_state = random_state
-        
+
         self.logger = logging.getLogger(__name__)
         if verbose_level == 2:
             self.logger.setLevel(logging.DEBUG)
@@ -21,76 +22,94 @@ class IRep:
         handler = logging.StreamHandler()
         handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
         self.logger.addHandler(handler)
-        self.logger.info(f"Initialized IRepPlusPlus with max_iterations={self.max_iterations}, max_conditions_in_rule={self.max_conditions_in_rule}, test_percentage={self.test_percentage:.2f}")
+        self.logger.info(f"Initialized IRep with max_iterations={self.max_iterations}, max_conditions_in_rule={self.max_conditions_in_rule}, test_percentage={self.test_percentage:.2f}")
+
+    def predict(self, X):
+        """Predict labels for the input data using the learned rules."""
+        predictions = np.zeros(len(X), dtype=int)
+
+        for rule in self.rule_sets:
+            predictions = predictions | self._apply_rule(rule, X)
+        return predictions
 
     def fit(self, x_original, y_original):
-        """Fit the IRep++ algorithm to the data."""
+        """Fit the IRep algorithm to the data."""
         self.rule_sets = []
 
         iterations = 0
         x = x_original.copy()
         y = y_original.copy()
 
-        while iterations != self.max_iterations:
+        while iterations != self.max_iterations and sum(y) != 0:
             X_train, X_prune, y_train, y_prune = train_test_split(x, y, test_size=self.test_percentage, random_state=self.random_state)
-            best_rule = self.learn_rule(X_train, y_train)
-            pruned_rule = self.prune_rule(best_rule, X_prune, y_prune)
-            self.logger.debug(f'\n---------------------------- ITERATION {iterations} -----------------------')
+            self.logger.debug(f'---------------------------- ITERATION {iterations} -----------------------')
+            self.logger.debug(f'Number of uncovered examples: {sum(y)}, in grow: {sum(y_train)}, in prune: {sum(y_prune)}')
+            best_rule = self._learn_rule(X_train, y_train)
+            pruned_rule = self._prune_rule(best_rule, X_prune, y_prune)
             self.logger.debug(f'Grow rule: {best_rule}')
             self.logger.debug(f'Pruned rule: {pruned_rule}')
 
-            if self.accept_rule(pruned_rule, X_prune, y_prune):
+            if pruned_rule != [] and self._accept_rule(pruned_rule, X_prune, y_prune):
                 self.rule_sets.append(pruned_rule)
-                covered_indices = self.apply_rule(pruned_rule, x)
+                covered_indices = self._apply_rule(pruned_rule, x)
                 x = x[~covered_indices]
                 y = y[~covered_indices]
+                self.logger.info(f'Added rule - {pruned_rule}')
             else:
                 self.logger.warning(f'Bad rule - {pruned_rule}, ending')
                 break
             iterations += 1
         self.logger.debug(f'Ending, {self.rule_sets=}')
 
-    def learn_rule(self, X, y):
+    def _learn_rule(self, x_original, y_original):
         """Grow rule """
-        best_rule = []
-        for _ in range(self.max_conditions_in_rule):
-            best_accuracy = 0
+        X = x_original.copy()
+        y = y_original.copy()
+        rule = []
+        iterations = 0
+        while sum(y) != 0 and iterations != self.max_conditions_in_rule:
+            best_gain = -1
             best_condition = None
             for feature in X.columns:
                 thresholds = X[feature].unique()
                 for threshold in thresholds:
                     for operator in ['<=', '>']:
                         condition = {"feature": feature, "threshold": threshold, "operator": operator}
-                        accuracy = accuracy_score(y, self.apply_rule([condition], X))
-                        if accuracy > best_accuracy:
-                            best_accuracy = accuracy
+                        inf_gain = self._learn_information_gain(rule, condition, x_original, y_original)
+                        if inf_gain > best_gain:
+                            best_gain = inf_gain
                             best_condition = condition
-            if best_condition not in best_rule:
-                best_rule.append(best_condition)
-            covered_indices = self.apply_rule(best_rule, X)
-            X = X[~covered_indices]
-            y = y[~covered_indices]
-        return best_rule
-
-    def prune_rule(self, rule, X, y):
-        """Prune the rule using a pruning set."""
-        pruned_rule = rule[:-1]
-        if self.evaluate_rule(pruned_rule, X, y) > self.evaluate_rule(rule, X, y):
-            return pruned_rule
+            if best_condition is not None and best_condition not in rule:
+                rule.append(best_condition)
+                covered_indices = self._apply_rule(rule, X)
+                X = X[~covered_indices]
+                y = y[~covered_indices]
+            iterations += 1
         return rule
 
-    def evaluate_rule(self, rule, X, y):
-        """Evaluate a rule by checking its accuracy on the given data."""
-        covered = np.ones(len(X), dtype=bool)
+    def _prune_rule(self, rule, X, y):
+        """Prune the rule using a pruning set."""
+        for _ in range(len(rule)):
+            pruned_rule = rule[:-1]
+            if self._evaluate_rule(pruned_rule, X, y) >= self._evaluate_rule(rule, X, y):
+                rule = pruned_rule
+            else:
+                return rule
+        return rule
 
-        for condition in rule:
-            covered = covered & self.apply_condition(X, condition)
+    def _learn_information_gain(self, current_rule, new_literal, x, y):
+        current_coverage = self._apply_rule(current_rule, x)
+        potential_coverage = self._apply_rule(current_rule + [new_literal], x)
+        p, n = sum(y[potential_coverage] == 1), sum(y[potential_coverage] == 0)
+        p0, n0 = sum(y[current_coverage] == 1), sum(y[current_coverage] == 0)
+        return self._calculate_information_gain(p, n, p0, n0)
 
-        if len(y[covered]) == 0:
-            return 0
-        return accuracy_score(y[covered], np.ones(len(y[covered])))
+    def _calculate_information_gain(self, p, n, p0, n0):
+        if p <= 0 or (p + n) <= 0 or p0 <= 0 or (p0 + n0) <= 0:
+            return 0.0
+        return p * (math.log2(p / (p + n)) - math.log2(p0 / (p0 + n0)))
 
-    def apply_condition(self, X, condition):
+    def _apply_condition(self, X, condition):
         """Apply a single condition to the dataset."""
         feature, threshold, operator = condition['feature'], condition['threshold'], condition['operator']
         if operator == '<=':
@@ -98,28 +117,25 @@ class IRep:
         else:
             return X[feature] > threshold
 
-    def apply_rule(self, rule, X):
+    def _apply_rule(self, rule, X):
         """Apply a rule (with multiple conditions) to the dataset."""
         covered = np.ones(len(X), dtype=bool)
         for condition in rule:
-            covered = covered & self.apply_condition(X, condition)
+            covered = covered & self._apply_condition(X, condition)
         return covered
+    
+    def _evaluate_rule(self, rule, X, y):
+        """Evaluate a rule by checking its accuracy on the given data."""
+        covered = np.ones(len(X), dtype=bool)
 
-    def apply_rule_set(self, rule_set, X):
-        covered = np.zeros(len(X), dtype=bool)
-        for condition in rule_set:
-            covered = covered | self.apply_condition(X, condition)
-        return covered
+        for condition in rule:
+            covered = covered & self._apply_condition(X, condition)
 
-    def predict(self, X):
-        """Predict labels for the input data using the learned rules."""
-        predictions = np.zeros(len(X), dtype=int)
+        if len(y[covered]) == 0:
+            return 0
+        return accuracy_score(y[covered], np.ones(len(y[covered])))
 
-        for rule in self.rule_sets:
-            predictions = predictions | self.apply_rule(rule, X)
-        return predictions
-
-    def accept_rule(self, rule, x_prune, y_prune):
-        rule_accuracy = self.evaluate_rule(rule, x_prune, y_prune)    
+    def _accept_rule(self, rule, x_prune, y_prune):
+        rule_accuracy = self._evaluate_rule(rule, x_prune, y_prune)    
         baseline_accuracy = accuracy_score(y_prune, np.zeros(len(y_prune)))
         return rule_accuracy > baseline_accuracy
--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 from irepplusplus import IRepPlusPlus
+from irep import IRep
 from get_data import read_csv_to_dataframe
 from sklearn.model_selection import train_test_split
 import sys
 
 
-def irepplusplus(file_path):
+def get_sets(file_path):
     data = read_csv_to_dataframe(file_path)
     X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['label']), data['label'], test_size=0.33, random_state=42)
+    return X_train, X_test, y_train, y_test
 
+
+def irepplusplus(X_train, X_test, y_train, y_test):
     model = IRepPlusPlus(verbose_level=2)
     model.fit(X_train, y_train)
 
@@ -15,7 +19,20 @@ def irepplusplus(file_path):
     correct_predictions = sum(predictions == y_test)
     final_accuracy = correct_predictions / len(y_test)
     print(f'Final accuracy: {final_accuracy}')
+    
+
+def irep(X_train, X_test, y_train, y_test):
+    irep_model = IRep(verbose_level=2)
+    irep_model.fit(X_train, y_train)
+    
+    predictions = irep_model.predict(X_test)
+    correct_predictions = sum(predictions == y_test)
+    final_accuracy = correct_predictions / len(y_test)
+    print(f'Final accuracy: {final_accuracy}')
 
 
 if __name__ == '__main__':
-    irepplusplus(sys.argv[1])
+    X_train, X_test, y_train, y_test = get_sets(sys.argv[1])
+    irepplusplus(X_train, X_test, y_train, y_test)
+
+    irep(X_train, X_test, y_train, y_test)
